d554b7ed8a7b8c490f4e886fba0af7787c92fe1f
samadi2 <satourdine@gmail.com>
2023-05-02 17:10:10 +0200

    ajout

--- src/pages/Recette.js
+++ src/pages/Recette.js
@@ -1,126 +1,70 @@
 // import Titre from "../components/Titre";
-import { useState, useEffect} from "react";
+import { useState} from "react";
 import {recipes} from '../components/recettes.js';
 import Titre from '../components/Titre.jsx';
 
 
+
 function Recette() {
-    
     const [searchTerm, setSearchTerm] = useState("");
 
-
-const handleSearchTerm = (e) => {
-    // console.log(e.target.value);
-    let value = e.target.value;
-    setSearchTerm(value);
-}
+    const handleSearchTerm = (e) => {
+        console.log(e.target.value);
+        let value = e.target.value;
+        setSearchTerm(value);
+    }
+    const recette = recipes.filter((food) => {
+        return food.name
+    });
     return (
-        <Titre text="Nos recettes"/>,
-
-    
-
-   
-        <>
-            <div className="searchBar">
-                <input
-                    type="text"
-                    name="searchBar"
-                    id="searchBar"
-                    placeholder="Rechercher"
-                    onChange={handleSearchTerm}
-                    />
+        <div>
+            <Titre text="Nos Recettes" />
+            <div className="searchbar">
+                <input className="recherche" id="searchbar" type="text" placeholder="Recherche" onChange={handleSearchTerm}/>
             </div>
-            <div className="searchresult">
-                {recipes.filter((food) => {
-                    // console.log(recipes.filter());
-                    return food.name.toLowerCase().includes(searchTerm.toLowerCase());
-                }).map((food) => {
+            <div className="Block">
+                {recette.length > 0 ? 
+                (
+                  recette.map((food) => 
+                    { 
+                        return(
+                            <div className="Block2">
+                                <div>
+                                    <img className="image2" src={require(`../JSON_img/img_recettes/${food.name}.jpg`)} alt={food.nom}/>
+                                </div>
+                                <h3>{food.name}</h3>
+                                <p>{food.description} </p>
+                                {
+                                    food.ingredients.map(ingre => {
+                                        return (
+                                            <div>
+                                                <p> ingredient : {ingre.ingredient} &emsp;  Quantité: {ingre.quantity} &emsp; Unité: {ingre.unit}</p>
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                        );
+                    })
+                ): 
+                  
+                (
 
-                    return(
-                        <div>
-                        <div>
-                        <img src={require(`../JSON_img/img_recettes/${food.name}.jpg`)} alt={food.nom}/>
-                        
+                    <p className="no-results">Aucune recette trouvée.</p>
+                )
+                }
+            </div>    
+        </div>  
+        
+    )
 
-                        </div>
-                        <h3>{food.name}</h3>
-                        <p>{food.description} </p>
-                            {
-                                food.ingredients.map(ingre => {
-                                    return (
-                                        <div>
-                                            <p> ingredient : {ingre.ingredient} &emsp;  Quantité: {ingre.quantity} &emsp; Unité: {ingre.unit}</p>
-                                        </div>
-                                    )
-                                })
-                            }
-                    </div>
-                    );
-                })}
-
-            </div>
-        </>
-    );
 }
         
 
 
 export default Recette ;
 
-// function Search(){
 
 
-//     const [searchTerm, setSearchTerm] = useState("");
 
 
-//     const handleSearchTerm = (e) => {
-//         console.log(e.target.value);
-//         let value = e.target.value;
-//         setSearchTerm(value);
-//     }
-
-//     console.log(searchTerm);
-
-//     return(
-//         <>
-//             <div className="searchBar">
-//                 <input
-//                     type="text"
-//                     name="searchBar"
-//                     id="searchBar"
-//                     placeholder="Rechercher"
-//                     onChange={handleSearchTerm}
-//                     />
-//             </div>
-//             <div className="searchresult">
-//                 {recipes.filter((food) => {
-//                     return food.name.toLowerCase().includes(searchTerm.toLowerCase());
-//                 }).map((food) => {
-//                     return(
-//                         <div>
-//                         <div>
-//                         <img src={require(`../JSON_img/img_recettes/${food.name}.jpg`)} alt={food.nom}/>
-                        
-
-//                         </div>
-//                         <h3>{food.name}</h3>
-//                         <p>{food.description} </p>
-//                             {
-//                                 food.ingredients.map(ingre => {
-//                                     return (
-//                                         <div>
-//                                             <p> ingredient : {ingre.ingredient} &emsp;  Quantité: {ingre.quantity} &emsp; Unité: {ingre.unit}</p>
-//                                         </div>
-//                                     )
-//                                 })
-//                             }
-//                     </div>
-//                     );
-//                 })}
-
-//             </div>
-//         </>
-//     );
-// }
-
-// export default Search;
